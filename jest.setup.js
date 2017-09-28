b5fbbc9f073974598cdb40929352ac6895424430
Eduardo Rabelo <oieduardorabelo@gmail.com>
2017-09-28 23:37:51 +1300

    core: React 15 deps and Enzyme adapter

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,4 @@
-import "raf/polyfill";
-
 import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import Adapter from "enzyme-adapter-react-15";
 
 Enzyme.configure({ adapter: new Adapter() });
